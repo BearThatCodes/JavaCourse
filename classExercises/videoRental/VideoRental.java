f453ed128977dbba8f3dcb42e8e14c8d0ec420c5
Isaac <izakizak@gmail.com>
2015-02-08 22:01:16 -0500

    End of lecture 2.

--- classExercises/videoRental/VideoRental.java
+++ classExercises/videoRental/VideoRental.java
@@ -1,7 +1,19 @@
+import java.util.Random;
+
 public class VideoRental{
 	public static void main(String argv[]){
         Video video;
-        video = new Video();
+        Video video2;
+        video = new Video("Citizen Kane",1941);
+        video2 = new Video("Blargh",3848);
         System.out.println(video);
+
+        Random ready = new Random();
+
+        for(int i=0;i<10;i++){
+            System.out.println(ready.nextInt());
+        }
+
+        System.out.println("Count: " + Video.getCount());
     }
 }
