781eeaa8d5f7b294e006d5b196d8af4381eff53a
Isaac <izakizak@gmail.com>
2015-04-15 22:12:24 -0400

    Implemented methods as described, need to test and fix.

---
+++ Project 2/hw5/src/cscie55/hw5/bank/BankServerImpl.java
@@ -0,0 +1,65 @@
+package cscie55.hw5.bank;
+
+import cscie55.hw5.bank.command.Command;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+/**
+ * Created by Isaac on 4/15/2015.
+ */
+public class BankServerImpl implements BankServer{
+    private Queue<Command> commandQueue;
+    private ArrayList<CommandExecutionThread> commandExecutionThreads;
+    private Bank bank;
+    private boolean executeCommandInsideMonitor;
+
+    public BankServerImpl(Bank bank,int numThreads,boolean executeCommandInsideMonitor) {
+        this.bank = bank;
+        this.executeCommandInsideMonitor = executeCommandInsideMonitor;
+
+        for(int i=0;i<numThreads;i++){
+            CommandExecutionThread threadToAdd = new CommandExecutionThread(bank,commandQueue,executeCommandInsideMonitor);
+            commandExecutionThreads.add(threadToAdd);
+            threadToAdd.start();
+        }
+    }
+
+    /**
+     * Executes the given Command.
+     *
+     * @param command the Command to be executed.
+     */
+    @Override
+    public void execute(Command command) {
+        synchronized (commandQueue){
+            commandQueue.add(command);
+        }
+    }
+
+    /**
+     * Stop the BankServer. No further Commands will be executed.
+     *
+     * @throws InterruptedException
+     */
+    @Override
+    public void stop() throws InterruptedException {
+        synchronized (commandExecutionThreads) {
+            for (int i = 0; i < commandExecutionThreads.size();) {
+                synchronized (commandQueue) {
+                    commandQueue.add(Command.stop());
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the total of all Account balances for all Accounts managed by the BankServer's Bank.
+     *
+     * @return The total of all Account balances for all Accounts managed by the BankServer's Bank.
+     */
+    @Override
+    public long totalBalances() {
+        return bank.totalBalances();
+    }
+}
---
+++ Project 2/hw5/src/cscie55/hw5/bank/CommandExecutionThread.java
@@ -0,0 +1,69 @@
+package cscie55.hw5.bank;
+
+import cscie55.hw5.bank.command.Command;
+
+import java.util.Queue;
+
+/**
+ * Created by Isaac on 4/15/2015.
+ */
+public class CommandExecutionThread extends Thread {
+    private Bank bank;
+    private Queue<Command> commandQueue;
+    private boolean executeCommandInsideMonitor;
+
+    public CommandExecutionThread(Bank bank,Queue<Command> commandQueue,boolean executeCommandInsideMonitor){
+        this.bank = bank;
+        this.commandQueue = commandQueue;
+        this.executeCommandInsideMonitor = executeCommandInsideMonitor;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void run() {
+        /*
+        This is to get around the fact that the compiler doesn't think we'll always reach
+        the statement where we pull a command off the Queue and initialize commandToRun
+         */
+        Command commandToRun = null;
+
+        synchronized (commandQueue) {
+            while (!commandQueue.peek().isStop()) {
+                while (commandQueue.size() <= 0){
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("Thread was interrupted.");
+                    }
+                }
+
+                commandToRun = commandQueue.remove();
+
+                if(executeCommandInsideMonitor) {
+                    try {
+                        commandToRun.execute(bank);
+                    }
+                    catch (InsufficientFundsException e){
+                        System.out.println(e);
+                    }
+                }
+            }
+
+            if(commandQueue.peek().isStop()){
+                commandQueue.remove();
+                return;
+            }
+        }
+
+        if(!executeCommandInsideMonitor && commandToRun != null){
+            try {
+                commandToRun.execute(bank);
+            }
+            catch (InsufficientFundsException e){
+                System.out.println(e);
+            }
+        }
+    }
+}
