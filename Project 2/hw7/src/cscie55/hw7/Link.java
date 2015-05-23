d5a75d03164b05b6948c2d7ea2c04945522e375e
Isaac <izakizak@gmail.com>
2015-05-23 11:00:15 -0400

    Starting last project.

---
+++ Project 2/hw7/src/cscie55/hw7/Link.java
@@ -0,0 +1,43 @@
+package cscie55.hw7;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Link implements Serializable
+{
+    public static Link parse(String line)
+    {
+        int urlTokenEnd = line.indexOf(URL_TOKEN) + URL_TOKEN.length();
+        int urlStart = line.indexOf(QUOTE, urlTokenEnd) + 1;
+        assert urlStart > urlTokenEnd : String.format("urlTokenEnd: %d, urlStart: %d", urlTokenEnd, urlStart);
+        int urlEnd = line.indexOf(QUOTE, urlStart);
+        assert urlEnd > urlStart;
+        String url = line.substring(urlStart, urlEnd);
+        int timestampTokenEnd = line.indexOf(TIMESTAMP_TOKEN, urlEnd) + TIMESTAMP_TOKEN.length();
+        int timestampStart = line.indexOf(SPACE, timestampTokenEnd);
+        // Get past consecutive spaces
+        while (line.charAt(timestampStart) == SPACE) {
+            timestampStart++;
+        }
+        int timestampEnd = line.indexOf(COMMA, timestampStart);
+        long timestamp = Long.parseLong(line.substring(timestampStart, timestampEnd));
+        int tagsTokenEnd = line.indexOf(TAGS_TOKEN, timestampEnd) + TAGS_TOKEN.length();
+        int startQuote;
+        int endQuote = tagsTokenEnd;
+        List<String> tags = new ArrayList<String>();
+        while ((startQuote = line.indexOf(QUOTE, endQuote + 1) + 1) != 0) {
+            endQuote = line.indexOf(QUOTE, startQuote);
+            String tag = line.substring(startQuote, endQuote);
+            tags.add(tag);
+        }
+        return new Link(url, timestamp, tags);
+    }
+
+    private static final String URL_TOKEN = "\"url\"";
+    private static final String TIMESTAMP_TOKEN = "\"timestamp\"";
+    private static final String TAGS_TOKEN = "\"tags\"";
+    private static final char QUOTE = '"';
+    private static final char SPACE = ' ';
+    private static final char COMMA = ',';
+}
